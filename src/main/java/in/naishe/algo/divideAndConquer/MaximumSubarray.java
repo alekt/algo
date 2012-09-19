a270e827af2e665b8a5ba1fb968a75b648ca3c60
Nishant Neeraj <nishant.neeraj@gmail.com>
2012-09-19 23:22:51 +0530

    added dummy files

---
+++ src/main/java/in/naishe/algo/divideAndConquer/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package in.naishe.algo.divideAndConquer;
+
+/**
+ * CLRS: Chapter 4 Divide-and-Conquer
+ * The maximum-subarray problem
+ * @author Nishant Neeraj (http://naishe.in)
+ *
+ */
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
