bc2c303cb0904a201c8a777494c14001f1642fa8
Nishant Neeraj <nishant.neeraj@gmail.com>
2012-10-02 20:05:08 +0530

    added quick sort

--- src/main/java/in/naishe/algo/utils/Util.java
+++ src/main/java/in/naishe/algo/utils/Util.java
@@ -20,4 +20,11 @@ public class Util {
 		}
 		System.out.println("]");
 	}
+
+	public static int[] swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		return a;
+	}
 }
