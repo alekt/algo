22623bd62d9c43ad01e9557f0f4666cf16a1fe87
Nishant Neeraj <nishant.neeraj@gmail.com>
2012-10-02 12:43:32 +0530

    added heap sort

---
+++ src/main/java/in/naishe/algo/utils/Util.java
@@ -0,0 +1,23 @@
+package in.naishe.algo.utils;
+
+import java.util.Random;
+
+public class Util {
+	public static final Random rand = new Random();
+	public static int[] generateIntArray(int length){
+		int[] a = new int[length];
+		for(int i=0; i<length; i++)
+			a[i] = rand.nextInt(100);
+		return a;
+	}
+	
+	public static void print(int[] a) {
+		System.out.print("[");
+		for(int i=0; i<a.length; i++){
+			System.out.print(a[i]);
+			if(i != a.length-1)
+				System.out.print(", ");
+		}
+		System.out.println("]");
+	}
+}
